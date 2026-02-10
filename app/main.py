5a04255b3e65449f4d62dbc46c55c9f0b0c29a6c
PYatiM <p.yathinmachaiah18@gmail.com>
2026-02-10 22:57:38 +0530

    putting openapi validator in main file

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from app.api.routes import router
 from app.core.config import settings
+from app.middleware.openapi_validator import OpenAPIValidatorMiddleware
 
 app = FastAPI(title=settings.APP_NAME)
 
 app.include_router(router)
+app.add_middleware(OpenAPIValidatorMiddleware)
 
 @app.get("/")
 def root():
