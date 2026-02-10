d89ae275aa0b884bd536fc34e8cf86c3089c2cc5
PYatiM <p.yathinmachaiah18@gmail.com>
2026-02-10 23:03:47 +0530

    modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from app.api.routes import router
 from app.core.config import settings
 from app.middleware.openapi_validator import OpenAPIValidatorMiddleware
+from app.middleware.rate_limit import RateLimitMiddleware
 
 app = FastAPI(title=settings.APP_NAME)
 
 app.include_router(router)
 app.add_middleware(OpenAPIValidatorMiddleware)
+app.add_middleware(RateLimitMiddleware)
 
 @app.get("/")
 def root():
